9fe4776199151889ad22a3b022527ad1601e3beb
Vincent <vinmorel@gmail.com>
2021-02-16 13:31:41 -0500

    adding tfidf/bow feature, adding stopwords, adding number pruning

--- aggregator/utils/preprocess.py
+++ aggregator/utils/preprocess.py
@@ -1,9 +1,11 @@
 import re
 import spacy
+import demoji
 import numpy as np
 from langdetect import detect
 from langdetect import DetectorFactory
-import demoji
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def check_dim(dim2_list: list) -> list:
@@ -35,8 +37,8 @@ def prune_punctuation(comment: str) -> str:
     for bad_punc in bad_punctuations:
         comment = re.sub(bad_punc, '', comment)
         
-    bad_punctuations2 =  [r"\s+:\)*\s?", r"\s+:\(*\s?", r"\s+\(:*\s?", r"\s+:p*\s?", r"\s+:d*\s?", 
-                            r"\s+:3*\s?", r"\s+\<3*\s?", r"\s+:o*\s?", r"\s+xd*\s?"]
+    bad_punctuations2 =  [r"(^|\s+):\)($|\s+)", r"(^|\s+):\(($|\s+)", r"(^|\s+)\(:($|\s+)", r"(^|\s+):p*($|\s+)", r"(^|\s+):d($|\s+)", 
+                            r"(^|\s+):3($|\s+)", r"(^|\s+)\<3($|\s+)", r"(^|\s+):o($|\s+)", r"(^|\s+)xd($|\s+)"]
     for bad_punc in bad_punctuations2:
         comment = re.sub(bad_punc, ' ', comment)
 
@@ -57,29 +59,43 @@ def prune_emojis(comment: str) -> str:
         comment = demoji.replace(comment, " ")
     return comment
 
+def prune_numbers(comment: str) -> str:
+    return re.sub(r'(^|\s+)[0-9]*\s?', ' ', comment)
+
 def fix_spaces(comment:str) -> str:
     return " ".join(comment.split())
 
 class lemmatizer():
+    """ lematizer object using spacy. Also removes stopwords. """
     def __init__(self, model: str = 'en_core_web_sm', batch_size: int = 100):
+        self.wdir = Path(__file__).resolve().parents[0]
+        self.stop_dir = self.wdir / "stopwords" / "additional_stopwords.txt"
+        self.add_sw = self.load_additional_stopwords(self.stop_dir)
         self.nlp = spacy.load(model)
         self.nlp.disable_pipes('ner')
         self.batch_size = batch_size
+        self.nlp.Defaults.stop_words.update(self.add_sw)
+
+    def load_additional_stopwords(self, stop_dir) -> list:
+        with open(stop_dir) as f:
+            more_stopwords = f.read().splitlines()
+            return more_stopwords
 
     def lemmatize(self, text: str):
         docs = self.nlp.pipe(text, batch_size=self.batch_size)
         lemmatized_comment_list = []
         for doc in docs:
-            lemmas = ' '.join([token.lemma_ for token in doc])
+            lemmas = ' '.join([token.lemma_ for token in doc if not token.is_stop]) # also remove stop words
             lemmatized_comment_list.append(lemmas)
         return lemmatized_comment_list
 
 
-def preprocess(comment_list: list) -> list:
+def preprocess(comment_list: list, vec: str = None) -> list:
     # Applies all preprocessing operations to extracted list of comments
+    # vec (str) : if tfidf' or 'bow', returns corresponding vectorization
     l = lemmatizer()
     comment_list = check_dim(comment_list)
-
+    
     preprocessed_comments = []
     for comment in comment_list:
         comment = prune_encodings(comment)
@@ -87,14 +103,23 @@ def preprocess(comment_list: list) -> list:
         comment = prune_links_emails(comment)
         comment = prune_punctuation(comment)
         comment = prune_emojis(comment)
+        comment = prune_numbers(comment)
         comment = fix_spaces(comment)
         preprocessed_comments.append(comment)
 
     preprocessed_comments = check_lang(preprocessed_comments)
-    print(preprocessed_comments)
     preprocessed_comments = l.lemmatize(preprocessed_comments)
 
+    if vec == 'tfidf':
+        vectorizer = TfidfVectorizer()
+        tfidf = vectorizer.fit_transform(preprocessed_comments)
+        return tfidf.toarray(), vectorizer.get_feature_names()
+    if vec == 'bow':
+        vectorizer = CountVectorizer()
+        bow = vectorizer.fit_transform(preprocessed_comments)
+        return bow.toarray(), vectorizer.get_feature_names()
     return preprocessed_comments
+
 
 if __name__ == "__main__":
     import pickle
@@ -107,5 +132,11 @@ if __name__ == "__main__":
         data = pickle.load(handle)
         comments = [i['snippet']['topLevelComment']['snippet']['textDisplay'] for i in data['items']]
     
+        # tfidf
+        preprocessed_comments, feature_names = preprocess(comments, vec='tfidf')
+
+        # bag of words
+        preprocessed_comments, feature_names = preprocess(comments, vec='bow')
+        
+        # standard 
         preprocessed_comments = preprocess(comments)
-        print(preprocessed_comments)
